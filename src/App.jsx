a4da29c6b6f1d75dcbfc4288382a5399a05c7115
Danny Berrios <denny_berrios@hotmail.com>
2025-01-14 22:44:17 -0300

    Inserindo a header, home e footer no arquivo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+
 const App = () => {
-  return ( 
-    <></>
-   );
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 }
- 
+
 export default App;
